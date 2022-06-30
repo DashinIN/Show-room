56510fc807f4a6b673adf1d7c4186a4e7e6b22bd
DashinIN <green19grant@yandex.ru>
2022-06-30 12:59:35 +0300

    Rework

--- docs/script/script.js
+++ docs/script/script.js
@@ -1,12 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
 
-	let a = document.querySelector(".logo");
-	a.classList.add("ready");
-
-});
 document.addEventListener('DOMContentLoaded', function() {
 
 	let a = document.querySelector(".logo-back");
 	a.classList.add("clear");
 
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+
+const audio = document.querySelectorAll('audio');
+console.log(audio);
+const items = document.querySelectorAll(".flexitem");
+console.log(items);
+
+    for (let i in items) {
+items[i].addEventListener("mouseenter", () => { console.log("enter"); audio[i].play();});
+items[i].addEventListener("mouseleave", () => { console.log("out"); audio[i].pause();});
+}
 });
