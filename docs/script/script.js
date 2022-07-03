61b828f687fad57ce54c415ab526da744d24b3fc
DashinIN <green19grant@yandex.ru>
2022-07-03 22:15:52 +0300

    npm add

--- docs/script/script.js
+++ docs/script/script.js
@@ -14,7 +14,7 @@ const items = document.querySelectorAll(".flexitem");
 console.log(items);
 
     for (let i in items) {
-items[i].addEventListener("mouseenter", () => { console.log("enter"); audio[i].play();});
-items[i].addEventListener("mouseleave", () => { console.log("out"); audio[i].pause();});
+items[i].addEventListener("mouseenter", () => { audio[i].play();});
+items[i].addEventListener("mouseleave", () => { audio[i].pause();});
 }
 });
