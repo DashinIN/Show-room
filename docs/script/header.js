2767878addfd63e22b8c8a0cf0a216d2441d7a07
DashinIN <green19grant@yandex.ru>
2022-07-08 17:07:40 +0300

    header and multipages

---
+++ docs/script/header.js
@@ -0,0 +1,37 @@
+
+const burgerMenu = document.querySelector(".header__burger");
+const headerNav = document.querySelector(".header__nav");
+if(burgerMenu) {
+    burgerMenu.addEventListener("click", function(e) {
+        document.body.classList.toggle("_lock");
+        burgerMenu.classList.toggle("_active");
+        headerNav.classList.toggle("_active");
+    })
+}
+
+// Переход по ссылкам
+
+const menuLinks = document.querySelectorAll(".header__nav-link[data-goto]");
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    })
+}
+
+function onMenuLinkClick(e) {
+    const menuLink = e.target;
+    if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+        const gotoBlock = document.querySelector(menuLink.dataset.goto);
+        const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY;
+    if (burgerMenu.classList.contains("_active")) {
+        document.body.classList.remove("_lock");
+        burgerMenu.classList.remove("_active");
+        headerNav.classList.remove("_active");
+    }
+    window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth"
+    });
+    }
+    e.preventDefault();
+}
---
+++ src/script/script.js
@@ -0,0 +1,19 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+
+	let a = document.querySelector(".logo-back");
+	a.classList.add("clear");
+
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+const audio = document.querySelectorAll('audio');
+const items = document.querySelectorAll(".flexitem");
+
+    for (let i in items) {
+items[i].addEventListener("focusin", () => { audio[i].play();});
+items[i].addEventListener("focusout", () => { audio[i].pause();});
+items[i].addEventListener("mouseenter", () => { audio[i].play();});
+items[i].addEventListener("mouseleave", () => { audio[i].pause();});
+}
+});
