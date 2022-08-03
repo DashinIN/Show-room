065095764a90143f945910414defc7e37079db1f
DashinIN <green19grant@yandex.ru>
2022-08-03 20:10:28 +0300

    4page stiky done

--- docs/header.js
+++ docs/header.js
@@ -125,3 +125,58 @@ let media =(volume) => {
         
     });
 
+
+    window.onload = function () {
+
+        const paralax = document.querySelector('.paralax');
+    
+        if (paralax) {
+ 
+            const headText = document.querySelector('.paralax__text');
+    
+         
+            const forText = 50;
+            
+            const speed = 0.1;
+    
+            let positionX = 0, positionY = 0;
+            let coordXpencent = 0, coordYpencent = 0;
+    
+    
+    
+    
+    
+            function setMouseParalaxStyle() {
+                const distX = coordXpencent - positionX;
+                const distY = coordYpencent - positionY;
+    
+                positionX+=distX*speed;
+                positionY+=distY*speed;
+    
+                
+                headText.style.cssText = `transform: translate(${-positionX/forText}%, ${-positionY/forText}%);`;
+    
+              requestAnimationFrame(setMouseParalaxStyle);
+    
+            }
+    
+            setMouseParalaxStyle();
+    
+            paralax.addEventListener('mousemove', function(e) {
+    
+                const paralaxWidth = paralax.offsetWidth;
+                const paralaxHeight = paralax.offsetHeight;
+    
+                const coordX = e.pageX- paralaxWidth/2;
+                const coordY = e.pageY - paralaxHeight/2;
+    
+                coordXpencent = coordX / paralaxWidth*100;
+                coordYpencent = coordY / paralaxHeight *100;
+                
+    
+            
+    
+            })
+        }
+    
+    }
